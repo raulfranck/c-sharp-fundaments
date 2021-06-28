aad088d225555739291bf96507fec9e4136d0f27
Raul Franck <raulengfranck@gmail.com>
2021-06-28 18:05:34 -0300

    Adicionar arquivos de projeto.

---
+++ CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    /// <summary>
+    /// <see cref=""/>
+    /// </summary>
+    class Comentarios {
+        public static void Executar() {
+            // Cuidado com os comentários desnecessários
+            Console.WriteLine("Codigo claro é sempre melhor!");
+
+            /*
+             * Comentário de várias linhas. 
+             */
+
+            Console.WriteLine("O C# tem o XML comments");
+        }
+    }
+}
---
+++ CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CursoCSharp.fundamentos {
+    class PrimeiroPrograma {
+        public static void Executar() {
+            Console.Write("Primeiro ");
+            Console.WriteLine("Programa");
+            Console.WriteLine("Terminou!");
+        }
+    }
+}
