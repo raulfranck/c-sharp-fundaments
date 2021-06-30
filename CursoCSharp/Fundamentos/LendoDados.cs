f7b56124fd27664244cc8e82ebaad043f75c088c
Raul Franck <raulengfranck@gmail.com>
2021-06-30 11:12:27 -0300

    Lendo dados do console / system.Globalization

---
+++ CursoCSharp/Fundamentos/LendoDados.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoCSharp.Fundamentos {
+    class LendoDados {
+        public static void Executar() {
+            Console.Write("Qual é o seu nome? ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Qual a sua idade? ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.Write("Qaul é o seu salário? ");
+            double salario = int.Parse(Console.ReadLine(),
+                CultureInfo.InvariantCulture); // Essa linha pede para desconsiderar notação de ponto e virgula do sistema operacional.
+                
+
+            Console.WriteLine($"{nome}, {idade}, R${salario}");
+        }
+    }
+}
