9e4afd6d29e7b626ebbd466c31253bca224f3e6b
Raul Franck <raulengfranck@gmail.com>
2021-07-13 18:33:31 -0300

    Construtores

---
+++ CursoCSharp/ClassesEMetodos/Membros.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    class Membros
+    {
+        public static void Executar()
+        {
+            Pessoa sicrano = new Pessoa();
+            sicrano.Nome = "Raul Franck";
+            sicrano.Idade = 27;
+
+            Console.WriteLine($"{sicrano.Nome}, {sicrano.Idade}");
+        }
+    }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using CursoCSharp.fundamentos;
 using CursoCSharp.Fundamentos;
+using CursoCSharp.EstruturaDeControle;
 
 namespace CursoCSharp {
     class Program {
@@ -20,6 +21,17 @@ namespace CursoCSharp {
                 {"Operadores Relacionais - Fundamentos", OperadoresRelacionais.Executar},
                 {"Operadores Logicos - Fundamentos", OperadoresLogicos.Executar},
                 {"Operadores Atribuição - Fundamentos", OperadoresAtribuicao.Executar},
+
+                // Estruturas de Controle
+                {"Estrutura If - Estrutura De Controle", EstruturaIf.Executar},
+                {"Estrutura If Else - Estrutura De Controle", EstruturaIfElse.Executar},
+                {"Estrutura If Else If - Estrutura De Controle", EstruturaIfeIfElse.Executar},
+                {"Estrutura Switch - Estrutura De Controle", EstruturaSwitch.Executar},
+                {"Estrutura While - Estrutura De Controle", EstruturaWhile.Executar},
+                {"Estrutura Do While - Estrutura De Controle", EstruturaDoWhile.Executar},
+                {"Estrutura For - Estrutura De Controle", EstruturaFor.Executar},
+                {"Estrutura ForEach - Estrutura De Controle", EstruturaForEach.Executar},
+                {"Usando o Break - Estrutura De Controle", Break.Executar},
             });
 
             central.SelecionarEExecutar();
